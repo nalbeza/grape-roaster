d01cdcc7685d4d198bf07d265cd9aa0cc3024315
Nicolas Albeza <n.albeza@gmail.com>
2014-09-22 11:09:02 +0200

    Fixed version problems

--- test/mappings/album.rb
+++ test/mappings/album.rb
@@ -1,5 +1,6 @@
 class AlbumMapping < Roaster::JsonApi::Mapping
   property :title
+  property :band
 
   collection :tracks
   #collection_representer class: Album
--- test/support/fixtures.rb
+++ test/support/fixtures.rb
@@ -1,4 +1,4 @@
 b = Band.create!(name: 'The Fugees')
-album = Album.create!(title: 'The Score')
+album = Album.create!(title: 'The Score', band: b)
 album.tracks << Track.create!(title: 'Ready or Not', index: 0)
 album.tracks << Track.create!(title: 'Killing Me Softly', index: 1)
